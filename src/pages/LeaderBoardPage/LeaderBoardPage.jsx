450ca2c6dc7bccd90b6126d8d517da2ac8eaf4e4
EkaterinaMikhalko <mikhalkoea@gmail.com>
2024-07-19 23:00:19 +0300

    cw_done

--- src/pages/LeaderBoardPage/LeaderBoardPage.jsx
+++ src/pages/LeaderBoardPage/LeaderBoardPage.jsx
@@ -3,6 +3,10 @@ import { Button } from "../../components/Button/Button";
 import styles from "./LeaderBoardPage.module.css";
 import { useEffect, useState } from "react";
 import { getLeaders } from "../../api";
+import achievement1 from "./images/achievement1.svg";
+import notAchievement1 from "./images/notAchievement1.svg";
+import achievement2 from "./images/achievement2.svg";
+import notAchievement2 from "./images/notAchievement2.svg";
 export function LeaderBoardPage() {
   const [leaders, setLeaders] = useState([]);
   useEffect(() => {
@@ -53,7 +57,18 @@ export function LeaderBoardPage() {
                 <p className={styles.leaderBoardText}>{leader.name}</p>
               </div>
               <div className={styles.colomn3}>
-                <p className={styles.leaderBoardText}>{leader.name}</p>
+                <div className={styles.leaderBoardAchievement}>
+                  {leader.achievements.includes(1) ? (
+                    <img src={achievement1} alt="achievement1" />
+                  ) : (
+                    <img src={notAchievement1} alt="notAchievement1" />
+                  )}
+                  {leader.achievements.includes(2) ? (
+                    <img src={achievement2} alt="achievement2" />
+                  ) : (
+                    <img src={notAchievement2} alt="notAchievement2" />
+                  )}
+                </div>
               </div>
 
               <div className={styles.colomn4}>
